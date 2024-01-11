3e1878e96e1d65010a3a01a1b076d18f43390280
Alli <adweavil@uncg.edu>
2024-01-11 16:30:22 -0500

    I made some changes

--- src/myjavaapp/MyJavaApp.java
+++ src/myjavaapp/MyJavaApp.java
@@ -10,6 +10,7 @@ public class MyJavaApp {
     
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Added A NEW LINE!!!");
     }
     
 }
