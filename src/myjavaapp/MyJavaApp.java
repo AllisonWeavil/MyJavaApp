0af3ffe3bf312d1f84e3435d243a380cebc54ec1
Alli <adweavil@uncg.edu>
2024-01-11 16:19:41 -0500

    initial commit

---
+++ src/myjavaapp/MyJavaApp.java
@@ -0,0 +1,15 @@
+
+package myjavaapp;
+
+/**
+ *
+ * @author alliw
+ */
+public class MyJavaApp {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
